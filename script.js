ddbc3a785de94ab17ef1803233b0729504f60bbf
Aman Sinha <sinhaaman2180@gmail.com>
2024-08-22 14:56:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function appendValue(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
